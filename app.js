f0205aaf5602095684c72384a07e81237f2c9f97
danielpoehle <d.poehle@gmx.de>
2017-09-09 18:32:29 +0200

    finished optional assignments

--- app.js
+++ app.js
@@ -11,23 +11,37 @@ LunchCheckController.$inject = ['$scope']; //if minified then the inject will st
 function LunchCheckController($scope){
   $scope.list = "";
   $scope.message = "";
+  $scope.alert = "text-muted";
 
   $scope.checkList = function () {
-    var number = $scope.list.split(',').length;
-    //console.log($scope.list.length);
+    var number = GetNumberOfNonEmptyItems($scope.list.split(','));
 
-    if ($scope.list.length === 0) {
+    if (number === 0) {
       $scope.message = "Please enter data first";
+      $scope.alert = "text-danger";
     }else {
       if (number <=3) {
         //$scope.message = number;
         $scope.message = "Enjoy!";
+        $scope.alert = "text-success";
       } else {
         //$scope.message = 2*number;
         $scope.message = "Too much!";
+        $scope.alert = "text-success";
       }
     }
   };
+
+  function GetNumberOfNonEmptyItems(list) {
+    var totalNumber = 0;
+    for (var i = 0; i < list.length; i++) {
+      if (list[i].length > 0) {
+        totalNumber = totalNumber+1;
+      }
+    }
+    //console.log(totalNumber);
+    return totalNumber;
+  }
 
 }
 
